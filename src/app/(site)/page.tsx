04513aa7fcb4c21a484d55d55cd547ea4005b801
Akshay Kamble <153273927+akshaykamble786@users.noreply.github.com>
2024-09-25 10:37:32 +0000

    build pricing section

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -4,30 +4,40 @@ import Image from "next/image";
 import React from "react";
 import Banner from "../../../public/appBanner.png";
 import cal from "../../../public/cal.png";
-import { CLIENTS, USERS } from "@/lib/constants";
+import Check from "../../../public/icons/check.svg";
+import Diamond from "../../../public/icons/diamond.svg";
+import { CLIENTS, PRICING_CARDS, USERS, PRICING_PLANS } from "@/lib/constants";
 import { randomUUID } from "crypto";
 import { twMerge } from "tailwind-merge";
 import clsx from "clsx";
 import { CustomCard } from "@/components/landing-page/custom-card";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { CardDescription, CardTitle } from "@/components/ui/card";
+import { CardContent, CardDescription, CardTitle } from "@/components/ui/card";
 
 const HomePage = () => {
   return (
     <>
       <section className="overflow-hidden px-4 sm:px-6 mt-10 sm:flex sm:flex-col gap-4 md:justify-center md:items-center">
         <TitleSection
-          pill="✨ Your workspace, Perfected"
-          title="Remember Nothing, Organize Everything"
+          pill="✨ Your workspace, Perfected with AI"
+          title="Take a backseat, let AI take control"
+          subheading="AI-Powered Planning and Collaboration at Your Fingertips"
         />
-        <div className="bg-white p-[2px] mt-6 rounded-xl bg-gradient-to-r from-primary to-brand-primaryBlue sm:w-[300px">
-          <Button className="w-full rounded-[10px] p-6 text-2xl bg-background">
-            Get Started
-          </Button>
+        <div className="flex gap-4">
+          <div className="bg-white p-[2px] mt-6 rounded-xl bg-gradient-to-r from-primary to-brand-primaryBlue sm:w-[200px]">
+            <Button className="w-full rounded-[10px] p-6 text-lg bg-background">
+              Get Started &rarr;
+            </Button>
+          </div>
+          <div className="bg-white p-[2px] mt-6 rounded-xl bg-gradient-to-r from-primary to-brand-primaryBlue sm:w-[200px]">
+            <Button className="w-full rounded-[10px] p-6 text-lg bg-background">
+              Read docs
+            </Button>
+          </div>
         </div>
         <div className="md:mt-[-90px] sm:w-full w-[750px] flex justify-center items-center mt-[-40px] relative sm:ml-0 ml-[-50px]">
           <Image src={Banner} alt="Application Banner" />
-          <div className="bottom-0 top-[-50%] bg-gradient-to-t dark:from-background left-0 right-0 absolute z-10"></div>
+          <div className="bottom-0 top-[-5%] bg-gradient-to-t dark:from-background left-0 right-0 absolute z-10"></div>
         </div>
       </section>
       <section className="relative">
@@ -57,58 +67,176 @@ const HomePage = () => {
       <section className="px-4 sm:px-6 flex justify-center items-center flex-col relatve">
         <div className="w-[30%] blur-[120px] rounded-full h-32 absolute bg-brand-primaryBlue/50 -z-10 top-22" />
         <TitleSection
-          title="Keep track of your meetings all in one place"
-          subheading="Capture your ideas, thoughts, and meeting notes in a structured and oranized manner"
+          title="Remember nothing, organize everything"
+          subheading="Capture your ideas, thoughts, and meeting notes in a structured and oranized manner with the help of AI"
           pill="Features"
         />
         <div className="mt-10 max-w-[450px] flex justify-center items-center relative sm:ml-0 rounded-2xl border-8 border-washed-purple-300 border-opacity-10">
           <Image src={cal} alt="Banner" className="rounded-2xl" />
         </div>
       </section>
-       <section className="relative">
+      <section className="relative">
         <div className="w-full blur-[120px] rounded-full h-32 absolute bg-brand-primaryPurple/50 -z-10 top-56"></div>
         <div className="mt-20 px-4 sm:px-6 flex flex-col overflow-x-hidden overflow-visible ">
           <TitleSection
-            title="Trusted by all"
+            title="Why Brainstorm?"
             subheading="Join thousands of satisfied users who rely on our platform for their 
-          personal and professional productivity needs."
+          personal and professional productivity needs"
             pill="Testimonials"
           />
           {[...Array(2)].map((arr, index) => (
             <div
               key={randomUUID()}
               className={twMerge(
-                clsx('mt-10 flex flex-nowrap gap-6 self-start', {
-                  'flex-row-reverse': index === 1,
-                  'animate-[slide_250s_linear_infinite]': true,
-                  'animate-[slide_250s_linear_infinite_reverse]': index === 1,
-                  'ml-[100vw]': index === 1,
+                clsx("mt-10 flex flex-nowrap gap-6 self-start", {
+                  "flex-row-reverse": index === 1,
+                  "animate-[slide_250s_linear_infinite]": true,
+                  "animate-[slide_250s_linear_infinite_reverse]": index === 1,
+                  "ml-[100vw]": index === 1,
                 }),
-                'hover:paused'
+                "hover:paused"
               )}
             >
               {USERS.map((testimonial, index) => (
-                <CustomCard key={testimonial.name} className="w-[500px] shrink-0s rounded-xl dark:bg-gradient-to-t dark:from-border dark:to-background" 
-                cardHeader={
-                  <div className="flex items-center gap-4">
-                    <Avatar>
-                      <AvatarImage src={`/avatars/${index+1}.png`}/>
-                      <AvatarFallback>AV</AvatarFallback>
-                    </Avatar>
+                <CustomCard
+                  key={testimonial.name}
+                  className="w-[500px] shrink-0s rounded-xl dark:bg-gradient-to-t dark:from-border dark:to-background"
+                  cardHeader={
+                    <div className="flex items-center gap-4">
+                      <Avatar>
+                        <AvatarImage src={`/avatars/${index + 1}.png`} />
+                        <AvatarFallback>AV</AvatarFallback>
+                      </Avatar>
                       <div>
-                        <CardTitle className="text-foreground">{testimonial.name}</CardTitle>
-                        <CardDescription className="dark:text-washed-purple-800">{testimonial.name.toLocaleUpperCase()}</CardDescription>
+                        <CardTitle className="text-foreground">
+                          {testimonial.name}
+                        </CardTitle>
+                        <CardDescription className="dark:text-washed-purple-800">
+                          {`@${testimonial.name.toLocaleLowerCase()}`}
+                        </CardDescription>
                       </div>
-                  </div>
-                }
-                cardContent={
-                  <p className="dark:text-washed-purple-800">{testimonial.message}</p>
-                }
-                >
-          
-                </CustomCard>
+                    </div>
+                  }
+                  cardContent={
+                    <p className="dark:text-washed-purple-800">
+                      {testimonial.message}
+                    </p>
+                  }
+                ></CustomCard>
               ))}
             </div>
+          ))}
+        </div>
+      </section>
+      <section
+        className="mt-20
+        px-4
+        sm:px-6
+      "
+      >
+        <TitleSection
+          title="Flexible Pricing for Every Need"
+          subheading="Choose the plan that best fits your project and budget"
+          pill="Pricing"
+        />
+        <div
+          className="flex 
+        flex-col-reverse
+        sm:flex-row
+        gap-4
+        justify-center
+        sm:items-stretch
+        items-center
+        mt-10
+        "
+        >
+          {PRICING_CARDS.map((card) => (
+            <CustomCard
+              key={card.planType}
+              className={clsx(
+                "w-[300px] rounded-2xl dark:bg-black/40 background-blur-3xl relative",
+                {
+                  "border-brand-primaryPurple/70":
+                    card.planType === PRICING_PLANS.proplan,
+                }
+              )}
+              cardHeader={
+                <CardTitle
+                  className="text-2xl
+                  font-semibold
+              "
+                >
+                  {card.planType === PRICING_PLANS.proplan && (
+                    <>
+                      <div
+                        className="hidden dark:block w-full blur-[120px] rounded-full h-32
+                        absolute
+                        bg-brand-primaryPurple/70
+                        -z-10
+                        top-0
+                      "
+                      />
+                      <Image
+                        src={Diamond}
+                        alt="Pro Plan Icon"
+                        className="absolute top-6 right-6"
+                      />
+                    </>
+                  )}
+                  {card.planType}
+                </CardTitle>
+              }
+              cardContent={
+                <CardContent className="p-0">
+                  <span
+                    className="font-normal 
+                    text-2xl
+                "
+                  >
+                    &#8377;{card.price}
+                  </span>
+                  {+card.price > 0 ? (
+                    <span className="dark:text-washed-purple-800 ml-1">
+                      /mo
+                    </span>
+                  ) : (
+                    ""
+                  )}
+                  <p className="dark:text-washed-purple-800">
+                    {card.description}
+                  </p>
+                  <Button className="whitespace-nowrap w-full mt-4">
+                    {card.planType === PRICING_PLANS.proplan
+                      ? "Go Pro"
+                      : "Get Started"}
+                  </Button>
+                </CardContent>
+              }
+              cardFooter={
+                <ul
+                  className="font-normal
+                  flex
+                  mb-2
+                  flex-col
+                  gap-4
+                "
+                >
+                  <small>{card.highlightFeature}</small>
+                  {card.freatures.map((feature) => (
+                    <li
+                      key={feature}
+                      className="flex
+                      items-center
+                      gap-2
+                    "
+                    >
+                      <Image src={Check} alt="Check Icon" />
+                      {feature}
+                    </li>
+                  ))}
+                </ul>
+              }
+            />
           ))}
         </div>
       </section>
--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,111 +1,144 @@
-import client1 from '../../public/client1.png';
-import client2 from '../../public/client2.png';
-import client3 from '../../public/client3.png';
-import client4 from '../../public/client4.png';
-import client5 from '../../public/client5.png';
+import client1 from "../../public/client1.png";
+import client2 from "../../public/client2.png";
+import client3 from "../../public/client3.png";
+import client4 from "../../public/client4.png";
+import client5 from "../../public/client5.png";
 
 export const CLIENTS = [
-    { alt: 'client1', logo: client1 },
-    { alt: 'client2', logo: client2 },
-    { alt: 'client3', logo: client3 },
-    { alt: 'client4', logo: client4 },
-    { alt: 'client5', logo: client5 },
+  { alt: "client1", logo: client1 },
+  { alt: "client2", logo: client2 },
+  { alt: "client3", logo: client3 },
+  { alt: "client4", logo: client4 },
+  { alt: "client5", logo: client5 },
 ];
 
 export const USERS = [
   {
-    name: 'Dev',
+    name: "Dev",
     message:
-      'Cypress has been a game-changer for our team. With its reliable end-to-end testing, we catch bugs early, leading to faster development cycles and improved collaboration.',
+      "Brainstorm has been a game-changer for our team. With its reliable end-to-end testing, we catch bugs early, leading to faster development cycles and improved collaboration.",
   },
   {
-    name: 'Arun',
+    name: "Arun",
     message:
-      "I used to spend hours debugging frontend issues, but Cypress simplified everything. Now, I'm more productive, and my colleagues can trust our code thanks to Cypress.",
+      "I used to spend hours debugging frontend issues, but Brainstorm simplified everything. Now, I'm more productive, and my colleagues can trust our code thanks to Brainstorm.",
   },
   {
-    name: 'Ayman',
+    name: "Ayman",
     message:
-      "Cypress has transformed the way we work. Our QA and development teams are on the same page, and our productivity has skyrocketed. It's a must-have tool.",
+      "Brainstorm has transformed the way we work. Our QA and development teams are on the same page, and our productivity has skyrocketed. It's a must-have tool.",
   },
   {
-    name: 'Nirmeet',
+    name: "Nirmeet",
     message:
-      'I was skeptical at first, but Cypress exceeded my expectations. Our project timelines have improved, and collaboration between teams is seamless.',
+      "I was skeptical at first, but Brainstorm exceeded my expectations. Our project timelines have improved, and collaboration between teams is seamless.",
   },
   {
-    name: 'Sahil',
+    name: "Sahil",
     message:
-      "Cypress made writing and running tests a breeze. Our team's productivity has never been higher, and we're delivering more reliable software.",
+      "Brainstorm made writing and running tests a breeze. Our team's productivity has never been higher, and we're delivering more reliable software.",
   },
   {
-    name: 'Shubham',
+    name: "Shubham",
     message:
-      "Thanks to Cypress, we've eliminated testing bottlenecks. Our developers and testers collaborate effortlessly, resulting in quicker releases.",
+      "Thanks to Brainstorm, we've eliminated testing bottlenecks. Our developers and testers collaborate effortlessly, resulting in quicker releases.",
   },
   {
-    name: 'Grace',
+    name: "Grace",
     message:
-      'Cypress has improved our development process significantly. We now have more time for innovation, and our products are of higher quality.',
+      "Brainstorm has improved our development process significantly. We now have more time for innovation, and our products are of higher quality.",
   },
   {
-    name: 'Hank',
+    name: "Hank",
     message:
-      "Cypress's user-friendly interface made it easy for our non-technical team members to contribute to testing. Our workflow is much more efficient now.",
+      "Brainstorm's user-friendly interface made it easy for our non-technical team members to contribute to testing. Our workflow is much more efficient now.",
   },
   {
-    name: 'Ivy',
+    name: "Ivy",
     message:
-      "Our team's collaboration improved immensely with Cypress. We catch issues early, leading to less friction and quicker feature deployments.",
+      "Our team's collaboration improved immensely with Brainstorm. We catch issues early, leading to less friction and quicker feature deployments.",
   },
   {
-    name: 'Jack',
+    name: "Jack",
     message:
-      "Cypress's robust testing capabilities have elevated our development standards. We work more harmoniously, and our releases are more reliable.",
+      "Brainstorm's robust testing capabilities have elevated our development standards. We work more harmoniously, and our releases are more reliable.",
   },
   {
-    name: 'Katherine',
+    name: "Katherine",
     message:
-      "Cypress is a lifesaver for our cross-functional teams. We're more productive, and there's a shared sense of responsibility for product quality.",
+      "Brainstorm is a lifesaver for our cross-functional teams. We're more productive, and there's a shared sense of responsibility for product quality.",
   },
   {
-    name: 'Liam',
+    name: "Liam",
     message:
-      "Cypress has helped us maintain high standards of quality. Our team's collaboration has improved, resulting in faster development cycles.",
+      "Brainstorm has helped us maintain high standards of quality. Our team's collaboration has improved, resulting in faster development cycles.",
   },
   {
-    name: 'Mia',
+    name: "Mia",
     message:
-      "Cypress is a powerful tool that improved our productivity and collaboration. It's now an integral part of our development process.",
+      "Brainstorm is a powerful tool that improved our productivity and collaboration. It's now an integral part of our development process.",
   },
   {
-    name: 'Nathan',
+    name: "Nathan",
     message:
-      "Cypress's user-friendly interface and detailed reporting have made testing a breeze. Our team's productivity is at an all-time high.",
+      "Brainstorm's user-friendly interface and detailed reporting have made testing a breeze. Our team's productivity is at an all-time high.",
   },
   {
-    name: 'Olivia',
+    name: "Olivia",
     message:
-      "We saw immediate benefits in terms of productivity and collaboration after adopting Cypress. It's an essential tool for our development workflow.",
+      "We saw immediate benefits in terms of productivity and collaboration after adopting Brainstorm. It's an essential tool for our development workflow.",
   },
   {
-    name: 'Paul',
+    name: "Paul",
     message:
-      "Cypress has streamlined our testing process and brought our teams closer. We're more efficient and deliver better results.",
+      "Brainstorm has streamlined our testing process and brought our teams closer. We're more efficient and deliver better results.",
   },
   {
-    name: 'Quinn',
+    name: "Quinn",
     message:
-      'Cypress has been a game-changer for us. Our productivity and collaboration have improved significantly, leading to better software.',
+      "Brainstorm has been a game-changer for us. Our productivity and collaboration have improved significantly, leading to better software.",
   },
   {
-    name: 'Rachel',
+    name: "Rachel",
     message:
-      'Thanks to Cypress, our testing process is now a seamless part of our development cycle. Our teams collaborate effortlessly.',
+      "Thanks to Brainstorm, our testing process is now a seamless part of our development cycle. Our teams collaborate effortlessly.",
   },
   {
-    name: 'Sam',
+    name: "Sam",
     message:
-      'Cypress is a fantastic tool that has revolutionized our workflow. Our productivity and collaboration have reached new heights.',
+      "Brainstorm is a fantastic tool that has revolutionized our workflow. Our productivity and collaboration have reached new heights.",
   },
 ];
+
+export const PRICING_CARDS = [
+  {
+    planType: "Starter Plan",
+    price: "0",
+    description: "Limited block trials  for teams",
+    highlightFeature: "",
+    freatures: [
+      "Five folders",
+      "AI-powered content generation",
+      "Chat with documents",
+      "Invite 2 guests",
+      "Real-time collaboration with live cursors",
+    ],
+  },
+  {
+    planType: "Pro Plan",
+    price: "59",
+    description: "Billed annually",
+    highlightFeature: "Everything in free +",
+    freatures: [
+      "Unlimited folders",
+      "Unlimited AI-powered content generation",
+      "Advanced chat with documents",
+      "Unlimited guests",
+      "Multilingual support",
+    ],
+  },
+];
+
+export const PRICING_PLANS = { proplan: "Pro Plan", freeplan: "Free Plan" };
+
+export const MAX_FOLDERS_FREE_PLAN = 5;
